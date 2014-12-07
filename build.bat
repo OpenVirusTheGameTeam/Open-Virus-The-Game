4e6e84a63c8381268765339ae0eb9e91c938b35c
unknown <Doma@wxp-nb-mbodnar.ad.navteq.com>
2014-12-07 14:52:51 +0100

    Test Commit + Build Script

---
+++ build.bat
@@ -0,0 +1,8 @@
+@echo off
+echo This is the build script.
+echo It will be what Jenkins will use to build the project.
+echo Everything in this batch file will be ran in this Workspace as CD.
+echo You can replace this text, but dont replace the Echo off statement.
+echo Thanks, Jan4V.
+echo.
+echo PS: For unity building i have a separate plugin in Jenkins.
